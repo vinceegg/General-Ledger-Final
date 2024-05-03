5d6f9c26f84c6b049cad58df08fd5f5640857670
meowicor <mrcr.macaraig3939@gmail.com>
2024-05-03 23:08:48 +0800

    merge to pataposna

--- resources/views/journals/LS.blade.php
+++ resources/views/journals/LS.blade.php
@@ -11,7 +11,7 @@
 <body>
 @csrf
 
-<!-- TOPNAV -->
+<!-- TOPNAVV -->
 
 <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
   <div class="px-3 py-3 lg:px-5 lg:pl-3">
