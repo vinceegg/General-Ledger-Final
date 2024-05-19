0e0a6b92b117618d65c03415135e224fdcb1a314
kamriesan <gangoso.mikaella@gmail.com>
2024-05-19 22:09:11 +0800

    mika:migrations

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -216,6 +216,10 @@
    </div>
 </aside>
 
+
+
+
+
 <div class="p-4 sm:ml-64">
    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
    <!-- TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com --> 
@@ -311,6 +315,135 @@
       </div> 
       </div>
    </div></div>
+
+
+
+
+
+<div class="grid sm:grid-cols-3 md:grid-cols-3 gap-4 mt-5">
+  <div class="col-span-2 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex gap-2 pb-3">
+    </div>
+    <div class="grid sm:grid-cols-3 md:grid-cols-4 gap-4">
+      First
+    </div>
+  </div>
+
+  <div class="col-span-2 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex gap-2 pb-3">
+    </div>
+    <div class="grid sm:grid-cols-3 md:grid-cols-4 gap-4">
+      First
+    </div>
+  </div>
+
+  <div class="col-span-2 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex gap-2 pb-3">
+    </div>
+    <div class="grid sm:grid-cols-1 md:grid-cols-4 gap-4">
+      First
+    </div>
+  </div>
+
+  <div class="pt-6 p-6 grid bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex gap-2 pb-3">
+      <div class="">
+        <svg class="w-6 h-6 text-blue-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M17 10H4a1 1 0 0 0-1 1v9m14-10a1 1 0 0 1 1 1m-1-1h-5.057M17 10a1 1 0 0 1 1 1m0 0v9m0 0a1 1 0 0 1-1 1m1-1a1 1 0 0 1-1 1m0 0H4m0 0a1 1 0 0 1-1-1m1 1a1 1 0 0 1-1 1m0 0V7m0 0a1 1 0 0 1 1-1h4.443a1 1 0 0 1 .8.4l2.7 3.6M3 7v3h8.943M18 18h2a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1h-5.057l-2.7-3.6a1 1 0 0 0-.8-.4H7a1 1 0 0 0-1 1v1"/>
+        </svg>
+      </div>
+      <div class="">
+        <text class="white-card-title">Debit and Credit Balance per Journal</text>
+      </div>
+    </div>
+
+    <!-- CHARTS -->
+    <div class="pt-3">
+      <canvas id="myChart"></canvas>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script>
+      const ctx = document.getElementById('myChart').getContext('2d');
+
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['CKDJ', 'CDJ', 'CRJ', 'GJ'],
+          datasets: [{
+              label: 'Debit',
+              data: [12, 19, 3, 5], // Debit data
+              backgroundColor: '#ff4949', // Tailwind CSS text-blue-800 color for Debit
+              borderWidth: 1
+            },
+            {
+              label: 'Credit',
+              data: [2, 3, 2, 3], // Credit data
+              backgroundColor: '#ffcc3d', // Tailwind CSS text-blue-500 color for Credit
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+
+      const config = {
+  type: 'line',
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart'
+      }
+    }
+  },
+};
+
+const DATA_COUNT = 7;
+const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
+
+const labels = Utils.months({count: 7});
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: Utils.numbers(NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.red,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+    },
+    {
+      label: 'Dataset 2',
+      data: Utils.numbers(NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.blue,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+    }
+  ]
+};
+
+    </script>
+  </div>
+</div>
+
+
+
+
+
+
+
+
 <div class="grid sm:grid-cols-1 md:grid-cols-3 gap-4 mt-5">
 <div class="col-span-2 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"> 
   <div class="flex gap-2 pb-3">
