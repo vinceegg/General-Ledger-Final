307dcf905597183afa4806942f49a3e844c95633
kamriesan <gangoso.mikaella@gmail.com>
2024-05-22 08:04:50 +0800

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -182,19 +182,6 @@
              @endforeach
          </li>
          <li>
-             @foreach([''] as $route) {{ $route }}
-             <a href="{{ url('/settings' . $route) }}" class="flex items-center p-2 text-white transition duration-75 rounded-lg hover:bg-blue-900 dark:hover:bg-gray-700 dark:text-white group">
-                 <svg class="w-5 h-5 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                     <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                         <path d="M19 11V9a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L12 2.757V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L2.929 4.343a1 1 0 0 0 0 1.414l.536.536L2.757 8H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535L8 17.243V18a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H18a1 1 0 0 0 1-1Z"/>
-                         <path d="M10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
-                     </g>
-                 </svg>
-                 <span class="ms-3">Settings</span>
-             </a>
-             @endforeach
-         </li>
-         <li>
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
                 <a href="{{ route('logout') }}" class="flex items-center p-2 text-white transition duration-75 rounded-lg hover:bg-blue-900 dark:hover:bg-gray-700 dark:text-white group"
@@ -605,6 +592,11 @@
       </div>
       <div class="grid sm:grid-cols-1 mb-10 md:grid-cols-2 gap-4 h-30" style="max-height: 500px; overflow-y: auto;">
         <livewire:recent-activities />
+        <!-- Second column content -->
+        <div>
+          <!-- Your content for the second column goes here -->
+          <livewire:recent-activities />
+        </div>
       </div>
     </div>
 
